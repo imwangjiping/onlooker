6ba605b3b9e8a5ceee1773da5306277af5507565
jwong <wang_jiping@hithinksoft.com>
2018-01-19 15:01:12 +0800

    Init Onlooker

--- app/src/main/java/com/imjwong/onlooker/repository/DemoRepository.java
+++ app/src/main/java/com/imjwong/onlooker/repository/DemoRepository.java
@@ -1,8 +1,13 @@
 package com.imjwong.onlooker.repository;
 
 import com.imjwong.onlooker.domain.DemoDomain;
+import com.imjwong.onlooker.domain.QDemoDomain;
+import com.querydsl.core.types.dsl.SimpleExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 
 /**
  * DemoRepository class
@@ -10,7 +15,11 @@ import org.springframework.data.querydsl.QueryDslPredicateExecutor;
  * @author J.Wong
  * @date 2018/01/19
  */
-public interface DemoRepository extends JpaRepository<DemoDomain, Long>, QueryDslPredicateExecutor<DemoDomain> {
+public interface DemoRepository extends JpaRepository<DemoDomain, Long>, QueryDslPredicateExecutor<DemoDomain>, QuerydslBinderCustomizer<QDemoDomain> {
 
-
+    @Override
+    default void customize(QuerydslBindings bindings, QDemoDomain demoDomain) {
+        bindings.bind(demoDomain.name).first(StringExpression::contains);
+        bindings.bind(demoDomain.id).first(SimpleExpression::eq);
+    }
 }
